8f50e562b30e18efdb32f917c9285b05901d371f
abdulhayevbahrom <abdulhayevbahromjon2002@gmail.com>
2025-05-23 14:32:37 +0500

    yangilandi

--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const multer = require("multer");
 const upload = multer();
+
 const upload2 = multer();
 
 // const adminController = require("../controller/adminController");
